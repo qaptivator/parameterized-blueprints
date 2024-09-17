3e89cc18f2fc8890fd8d74f6c7e3e054e651d2b2
qaptivator <ac@i-ns.net>
2024-09-17 12:56:07 +0300

    new placeholders + productivity limitation check

--- data-updates.lua
+++ data-updates.lua
@@ -1,13 +1,13 @@
 for _, value in pairs(data.raw["assembling-machine"]) do
-    if value.crafting_categories then
-        value.crafting_categories[#value.crafting_categories+1] = "parameters-category"
-    end
+	if value.crafting_categories then
+		value.crafting_categories[#value.crafting_categories + 1] = "parameters-category"
+	end
 end
 
 for _, value in pairs(data.raw["module"]) do
-    if value.name:find("productivity%-module") then
-        for i = 0, 9, 1 do
-            value.limitation[#value.limitation+1] = "placeholder-" .. i
-        end
-      end
+	if value.name:find("productivity%-module") and value.limitaion then
+		for i = 0, 9, 1 do
+			value.limitation[#value.limitation + 1] = "placeholder-" .. i
+		end
+	end
 end
