a4ebb9e0bbe480ad3d883135f77c9af4a0228525
qaptivator <ac@i-ns.net>
2024-09-17 12:58:22 +0300

    fix crash when pressing shortcut hotkey

--- script/blueprints.lua
+++ script/blueprints.lua
@@ -6,96 +6,114 @@ local blueprints = {}
 
 blueprints.placeholder_pattern_string = "placeholder%-[0-9]$"
 
-blueprints.get_held_blueprint = function (player)
-    if not player.is_cursor_blueprint() then return nil end
+blueprints.get_held_blueprint = function(player)
+	if not player.is_cursor_blueprint() then
+		return nil
+	end
 
-    -- if the held item is a blueprint (item variant) then check if it's setup before doing anything with it
-    if player.cursor_stack.valid_for_read then
-        if player.cursor_stack.type == "blueprint" and not player.cursor_stack.is_blueprint_setup() then
-            return nil
-        end
-    end
+	-- if the held item is a blueprint (item variant) then check if it's setup before doing anything with it
+	if player.cursor_stack.valid_for_read then
+		if player.cursor_stack.type == "blueprint" and not player.cursor_stack.is_blueprint_setup() then
+			return nil
+		end
+	end
 
-    local blueprint = {
-        entities = player.get_blueprint_entities(),
-        tiles = {},
-        is_item = player.cursor_stack.valid_for_read,
-        item = nil,
-    }
+	local blueprint = {
+		entities = player.get_blueprint_entities(),
+		tiles = {},
+		is_item = player.cursor_stack.valid_for_read,
+		item = nil,
+	}
 
-    -- ensure that the table exists - a blueprint with only times will return nil on get_blueprint entities
-    if blueprint.entities == nil then
-        blueprint.entities = {}
-    end
+	-- ensure that the table exists - a blueprint with only times will return nil on get_blueprint entities
+	if blueprint.entities == nil then
+		blueprint.entities = {}
+	end
 
-    if blueprint.is_item then
-        blueprint.item = player.cursor_stack
+	if blueprint.is_item then
+		blueprint.item = player.cursor_stack
 
-        -- check if it's an item first - if it's from a book then get_blueprint_tiles is invalid
-        if blueprint.item.type == "blueprint" then
-            blueprint.tiles = player.cursor_stack.get_blueprint_tiles()
-        end
-    end
+		-- check if it's an item first - if it's from a book then get_blueprint_tiles is invalid
+		if blueprint.item.type == "blueprint" then
+			blueprint.tiles = player.cursor_stack.get_blueprint_tiles()
+		end
+	end
 
-    return blueprint
+	return blueprint
 end
 
-blueprints.get_parameterization_info = function (blueprint)
-    local info = {
-        is_parameterizable = false,
-        uses_0 = false,
-        uses_1 = false,
-        uses_2 = false,
-        uses_3 = false,
-        uses_4 = false,
-        uses_5 = false,
-        uses_6 = false,
-        uses_7 = false,
-        uses_8 = false,
-        uses_9 = false,
-    }
+blueprints.get_parameterization_info = function(blueprint)
+	local info = {
+		is_parameterizable = false,
+		uses_0 = false,
+		uses_1 = false,
+		uses_2 = false,
+		uses_3 = false,
+		uses_4 = false,
+		uses_5 = false,
+		uses_6 = false,
+		uses_7 = false,
+		uses_8 = false,
+		uses_9 = false,
+	}
 
-    for _, entity in pairs(blueprint.entities) do
-        entities.check_for_placeholder_values(entity, info)
-    end
+	for _, entity in pairs(blueprint.entities) do
+		entities.check_for_placeholder_values(entity, info)
+	end
 
-    return info
+	return info
 end
 
-blueprints.modify_blueprint = function (base_blueprint, changes)
-    local result = util.table.deepcopy(base_blueprint)
+blueprints.modify_blueprint = function(base_blueprint, changes)
+	local result = util.table.deepcopy(base_blueprint)
 
-    for _, entity in pairs(result.entities) do
-        entities.replace_placeholder_value(entity, changes)
-    end
+	if result == nil then
+		return
+	end
 
-    return result
+	for _, entity in pairs(result.entities) do
+		entities.replace_placeholder_value(entity, changes)
+	end
+
+	return result
 end
 
-blueprints.give_player_blueprint = function (player, blueprint)
-    local temp_inventory = game.create_inventory(1)
-    local blueprint_item = temp_inventory[1]
-    blueprint_item.set_stack({name = "blueprint"})
+blueprints.give_player_blueprint = function(player, blueprint)
+	if blueprint == nil then
+		return
+	end
 
-    if blueprint.entities then blueprint_item.set_blueprint_entities(blueprint.entities) end
-    if blueprint.tiles then blueprint_item.set_blueprint_tiles(blueprint.tiles) end
+	local temp_inventory = game.create_inventory(1)
+	local blueprint_item = temp_inventory[1]
+	blueprint_item.set_stack({ name = "blueprint" })
 
-    player.add_to_clipboard(blueprint_item)
-    player.activate_paste()
+	if blueprint.entities then
+		blueprint_item.set_blueprint_entities(blueprint.entities)
+	end
+	if blueprint.tiles then
+		blueprint_item.set_blueprint_tiles(blueprint.tiles)
+	end
 
-    temp_inventory.destroy()
+	player.add_to_clipboard(blueprint_item)
+	player.activate_paste()
+
+	temp_inventory.destroy()
 end
 
-blueprints.backfill_changes = function (changes)
-    for i = 0, 9, 1 do
-        local name = "placeholder-" .. i
-        if changes.recipes[name] == nil then
-            changes.recipes[name] = nil
-        end
-        if changes.signals[name] == nil then
-            changes.signals[name] = { type = "item", name = nil }
-        end
-    end
+blueprints.backfill_changes = function(changes)
+	if changes == nil then
+		return
+	end
+
+	for i = 0, 9, 1 do
+		local name = "placeholder-" .. i
+		if changes.recipes[name] == nil then
+			changes.recipes[name] = nil
+		end
+		if changes.signals[name] == nil then
+			changes.signals[name] = { type = "item", name = nil }
+		end
+	end
 end
 
 return blueprints
--- script/gui.lua
+++ script/gui.lua
@@ -1,6 +1,6 @@
 local mod_gui = require("mod-gui")
 local globalstore = require("script.globalstore")
-local blueprints  = require("script.blueprints")
+local blueprints = require("script.blueprints")
 
 local gui = {}
 
@@ -9,289 +9,307 @@ gui.window_close_button_name = "blueprint_parameter_window_close_button"
 gui.confirm_parameters_button_name = "confirm_parameters_button"
 gui.options_content_flow_name = "options_content_flow"
 
-gui.create_options_gui = function (player, blueprint, info)
-    -- close an already open options gui
-    gui.close_options_gui(player.index)
+gui.create_options_gui = function(player, blueprint, info)
+	-- close an already open options gui
+	gui.close_options_gui(player.index)
 
-    local frame = {
-        type = "frame",
-        direction = "vertical",
-        name = gui.window_name,
-        style = mod_gui.frame_style
-    }
-    frame = mod_gui.get_frame_flow(player).add(frame)
-    globalstore.set_value(player.index, "options_gui", frame)
-    globalstore.set_value(player.index, "last_selected_blueprint", blueprint)
+	local frame = {
+		type = "frame",
+		direction = "vertical",
+		name = gui.window_name,
+		style = mod_gui.frame_style,
+	}
+	frame = mod_gui.get_frame_flow(player).add(frame)
+	globalstore.set_value(player.index, "options_gui", frame)
+	globalstore.set_value(player.index, "last_selected_blueprint", blueprint)
 
-    local header = frame.add({
-        type = "flow",
-        direction = "horizontal",
-    })
+	local header = frame.add({
+		type = "flow",
+		direction = "horizontal",
+	})
 
-    local title = header.add({
-        type = "label",
-        caption = {"parameterized-blueprints-gui-options-title"}
-    })
+	local title = header.add({
+		type = "label",
+		caption = { "parameterized-blueprints-gui-options-title" },
+	})
 
-    local spacer = header.add({
-        type = "empty-widget",
-    })
-    spacer.style.height = 24
-    spacer.style.horizontally_stretchable = true
+	local spacer = header.add({
+		type = "empty-widget",
+	})
+	spacer.style.height = 24
+	spacer.style.horizontally_stretchable = true
 
-    if blueprint.is_item then
-    local close_button = header.add({
-            type = "sprite-button",
-            name = gui.window_close_button_name,
-            style = "frame_action_button",
-            sprite = "utility/close_white",
-            hovered_sprite = "utility/close_black",
-            clicked_sprite = "utility/close_black",
-            --tooltip = { "parameterized-blueprints.gui.close_tooltip" }
-            tooltip = { "gui.close-instruction" }
-        })
-    end
+	if blueprint.is_item then
+		local close_button = header.add({
+			type = "sprite-button",
+			name = gui.window_close_button_name,
+			style = "frame_action_button",
+			sprite = "utility/close_white",
+			hovered_sprite = "utility/close_black",
+			clicked_sprite = "utility/close_black",
+			--tooltip = { "parameterized-blueprints.gui.close_tooltip" }
+			tooltip = { "gui.close-instruction" },
+		})
+	end
 
-    local content_frame = frame.add({
-        type = "frame",
-        direction = "vertical",
-        name = "frame",
-        style = "inside_shallow_frame_with_padding",
-    })
+	local content_frame = frame.add({
+		type = "frame",
+		direction = "vertical",
+		name = "frame",
+		style = "inside_shallow_frame_with_padding",
+	})
 
-    local content_flow = content_frame.add({
-        type = "flow",
-        direction = "vertical",
-        name = gui.options_content_flow_name,
-    })
+	local content_flow = content_frame.add({
+		type = "flow",
+		direction = "vertical",
+		name = gui.options_content_flow_name,
+	})
 
-    for i = 0, 9, 1 do
-        local show = info["uses_" .. i]
-        if show then
-            gui.create_parameter_option(content_flow, i)
-        end
-    end
+	for i = 0, 9, 1 do
+		local show = info["uses_" .. i]
+		if show then
+			gui.create_parameter_option(content_flow, i)
+		end
+	end
 
-    local footer = frame.add({
-        type = "flow",
-        direction = "horizontal",
-        style = "dialog_buttons_horizontal_flow"
-    })
+	local footer = frame.add({
+		type = "flow",
+		direction = "horizontal",
+		style = "dialog_buttons_horizontal_flow",
+	})
 
-    local footer_spacer = footer.add({
-        type = "empty-widget"
-    })
-    footer_spacer.style.horizontally_stretchable = true
+	local footer_spacer = footer.add({
+		type = "empty-widget",
+	})
+	footer_spacer.style.horizontally_stretchable = true
 
-    footer.add({
-        type = "button",
-        caption = {"parameterized-blueprints-gui-create-blueprint"},
-        style = "confirm_button",
-        name = gui.confirm_parameters_button_name
-    })
+	footer.add({
+		type = "button",
+		caption = { "parameterized-blueprints-gui-create-blueprint" },
+		style = "confirm_button",
+		name = gui.confirm_parameters_button_name,
+	})
 
-    return frame
+	return frame
 end
 
-gui.is_options_open = function (player_index)
-    local result = globalstore.get_value(player_index, "options_gui")
-    if result then
-        return result.valid
-    end
-    return false
+gui.is_options_open = function(player_index)
+	local result = globalstore.get_value(player_index, "options_gui")
+	if result then
+		return result.valid
+	end
+	return false
 end
 
-gui.close_options_gui = function (player_index)
-    if gui.is_options_open(player_index) then
-        globalstore.get_value(player_index, "options_gui").destroy()
-        globalstore.set_value(player_index, "options_gui", nil)
-    end
+gui.close_options_gui = function(player_index)
+	if gui.is_options_open(player_index) then
+		globalstore.get_value(player_index, "options_gui").destroy()
+		globalstore.set_value(player_index, "options_gui", nil)
+	end
 end
 
-gui.create_parameter_option = function (root, index)
-    local flow = root.add({
-        type = "flow",
-        direction = "horizontal",
-        name = "placeholder-" .. index
-    })
-    flow.style.vertical_align = "center"
+gui.create_parameter_option = function(root, index)
+	local flow = root.add({
+		type = "flow",
+		direction = "horizontal",
+		name = "placeholder-" .. index,
+	})
+	flow.style.vertical_align = "center"
 
-    local placeholder_showcase_button = flow.add({
-        type = "choose-elem-button",
-        style = "slot_button_in_shallow_frame",
-        elem_type = "signal",
-    })
-    placeholder_showcase_button.elem_value = { type = "item", name = "placeholder-" .. index }
-    placeholder_showcase_button.locked = true
+	local placeholder_showcase_button = flow.add({
+		type = "choose-elem-button",
+		style = "slot_button_in_shallow_frame",
+		elem_type = "signal",
+	})
+	placeholder_showcase_button.elem_value = { type = "item", name = "placeholder-" .. index }
+	placeholder_showcase_button.locked = true
 
-    local sprite = flow.add({
-        type = "sprite",
-        sprite = "parameter-gui-indicator-arrow",
-        resize_to_sprite = false,
-    })
-    sprite.style.width = 16
-    sprite.style.height = 16
+	local sprite = flow.add({
+		type = "sprite",
+		sprite = "parameter-gui-indicator-arrow",
+		resize_to_sprite = false,
+	})
+	sprite.style.width = 16
+	sprite.style.height = 16
 
-    local select_item_button = flow.add({
-        type = "choose-elem-button",
-        style = "slot_button_in_shallow_frame",
-        name = "target_item_button",
-        elem_type = "signal",
-        -- signals don't support filters
-    })
+	local select_item_button = flow.add({
+		type = "choose-elem-button",
+		style = "slot_button_in_shallow_frame",
+		name = "target_item_button",
+		elem_type = "signal",
+		-- signals don't support filters
+	})
 
-    local select_recipe_button = flow.add({
-        type = "choose-elem-button",
-        style = "slot_button_in_shallow_frame",
-        name = "target_recipe_button",
-        elem_type = "recipe",
-        visible = false,
-    })
+	local select_recipe_button = flow.add({
+		type = "choose-elem-button",
+		style = "slot_button_in_shallow_frame",
+		name = "target_recipe_button",
+		elem_type = "recipe",
+		visible = false,
+	})
 end
 
-gui.handle_select_signal_button_changed = function (event)
-    if event.element.name ~= "target_item_button" and event.element.name ~= "target_recipe_button" then
-        return
-    end
+gui.handle_select_signal_button_changed = function(event)
+	if event.element.name ~= "target_item_button" and event.element.name ~= "target_recipe_button" then
+		return
+	end
 
-    -- only allow target_item_button to continue
-    if event.element.name ~= "target_item_button" then
-        gui.confirm_selection_if_all_are_set(event.player_index)
-        return
-    end
+	-- only allow target_item_button to continue
+	if event.element.name ~= "target_item_button" then
+		gui.confirm_selection_if_all_are_set(event.player_index)
+		return
+	end
 
-    if event.element.elem_value and string.match(event.element.elem_value.name, blueprints.placeholder_pattern_string) then
-        event.element.elem_value = nil
-        game.players[event.player_index].print("placeholder items are not valid here (these cannot be hidden from the ui)")
-        return
-    end
+	if
+		event.element.elem_value and string.match(event.element.elem_value.name, blueprints.placeholder_pattern_string)
+	then
+		event.element.elem_value = nil
+		game.players[event.player_index].print(
+			"placeholder items are not valid here (these cannot be hidden from the ui)"
+		)
+		return
+	end
 
-    local signal_button = event.element
-    local recipe_button = event.element.parent["target_recipe_button"]
-    if signal_button.elem_value then
-        if signal_button.elem_value.type == "item" then
-            gui.set_recipe_filters(signal_button.elem_value, recipe_button, "item")
-        elseif signal_button.elem_value.type == "fluid" then
-            gui.set_recipe_filters(signal_button.elem_value, recipe_button, "fluid")
-        else
-            recipe_button.visible = false
-        end
-    else
-        recipe_button.visible = false
-    end
+	local signal_button = event.element
+	local recipe_button = event.element.parent["target_recipe_button"]
+	if signal_button.elem_value then
+		if signal_button.elem_value.type == "item" then
+			gui.set_recipe_filters(signal_button.elem_value, recipe_button, "item")
+		elseif signal_button.elem_value.type == "fluid" then
+			gui.set_recipe_filters(signal_button.elem_value, recipe_button, "fluid")
+		else
+			recipe_button.visible = false
+		end
+	else
+		recipe_button.visible = false
+	end
 
-    gui.confirm_selection_if_all_are_set(event.player_index)
+	gui.confirm_selection_if_all_are_set(event.player_index)
 end
 
-gui.set_recipe_filters = function (signal, recipe_button, type)
-    local recipes = game.get_filtered_recipe_prototypes{
-        { filter = "hidden", invert = true, mode = "and" },
-        { filter = "has-product-" .. type, elem_filters = {
-            { filter = "name", name = signal.name }
-        }, mode = "and" }
-    }
+gui.set_recipe_filters = function(signal, recipe_button, type)
+	local recipes = game.get_filtered_recipe_prototypes({
+		{ filter = "hidden", invert = true, mode = "and" },
+		{
+			filter = "has-product-" .. type,
+			elem_filters = {
+				{ filter = "name", name = signal.name },
+			},
+			mode = "and",
+		},
+	})
 
-    if #recipes == 0 then
-        recipe_button.elem_value = nil
-        recipe_button.visible = false
-    elseif #recipes == 1 then
-        local recipe = nil
-        for key, _ in pairs(recipes) do
-            recipe = key
-            break
-        end
-        recipe_button.elem_value = recipe
-        recipe_button.visible = false
-    else
-        recipe_button.elem_value = nil
-        recipe_button.elem_filters = {
-            { filter = "hidden", invert = true, mode = "and" },
-            { filter = "subgroup", subgroup = "parameters-recipes" , invert = "true" , mode = "and" },
-            { filter = "has-product-" .. type, elem_filters = {
-                { filter = "name", name = signal.name }
-            }, mode = "and" }
-        }
-        recipe_button.visible = true
-    end
+	if #recipes == 0 then
+		recipe_button.elem_value = nil
+		recipe_button.visible = false
+	elseif #recipes == 1 then
+		local recipe = nil
+		for key, _ in pairs(recipes) do
+			recipe = key
+			break
+		end
+		recipe_button.elem_value = recipe
+		recipe_button.visible = false
+	else
+		recipe_button.elem_value = nil
+		recipe_button.elem_filters = {
+			{ filter = "hidden", invert = true, mode = "and" },
+			{ filter = "subgroup", subgroup = "parameters-recipes", invert = "true", mode = "and" },
+			{
+				filter = "has-product-" .. type,
+				elem_filters = {
+					{ filter = "name", name = signal.name },
+				},
+				mode = "and",
+			},
+		}
+		recipe_button.visible = true
+	end
 end
 
-gui.get_changes_from_ui = function (player_index)
-    if not gui.is_options_open(player_index) then return nil end
+gui.get_changes_from_ui = function(player_index)
+	if not gui.is_options_open(player_index) then
+		return nil
+	end
 
-    local changes = {
-        recipes = {},
-        signals = {},
-        items = {},
-        fluids = {},
-    }
+	local changes = {
+		recipes = {},
+		signals = {},
+		items = {},
+		fluids = {},
+	}
 
-    local entry_frame = globalstore.get_value(player_index, "options_gui")["frame"][gui.options_content_flow_name]
-    for index, entry in ipairs(entry_frame.children) do
-        -- don't check for visiblity - will need to grab value stored here even if it's not visible
-        local recipe_button = entry["target_recipe_button"]
-        local item_button = entry["target_item_button"]
-        if recipe_button.elem_value then
-            changes.recipes[entry.name] = recipe_button.elem_value
-        end
-        changes.signals[entry.name] = item_button.elem_value
-        if item_button.elem_value then
-            if item_button.elem_value.type == "item" then
-                changes.items[entry.name] = item_button.elem_value.name
-            end
-            if item_button.elem_value.type == "fluid" then
-                changes.fluids[entry.name] = item_button.elem_value.name
-            end
-        end
-    end
+	local entry_frame = globalstore.get_value(player_index, "options_gui")["frame"][gui.options_content_flow_name]
+	for index, entry in ipairs(entry_frame.children) do
+		-- don't check for visiblity - will need to grab value stored here even if it's not visible
+		local recipe_button = entry["target_recipe_button"]
+		local item_button = entry["target_item_button"]
+		if recipe_button.elem_value then
+			changes.recipes[entry.name] = recipe_button.elem_value
+		end
+		changes.signals[entry.name] = item_button.elem_value
+		if item_button.elem_value then
+			if item_button.elem_value.type == "item" then
+				changes.items[entry.name] = item_button.elem_value.name
+			end
+			if item_button.elem_value.type == "fluid" then
+				changes.fluids[entry.name] = item_button.elem_value.name
+			end
+		end
+	end
 
-    return changes
+	return changes
 end
 
 gui.confirm_selection = function(player_index)
-    local player = game.players[player_index]
-    local changes = gui.get_changes_from_ui(player_index)
-    blueprints.backfill_changes(changes)
-    local base_blueprint = globalstore.get_value(player_index, "last_selected_blueprint")
-    local modified_blueprint = blueprints.modify_blueprint(base_blueprint, changes)
-    blueprints.give_player_blueprint(player, modified_blueprint)
-    gui.close_options_gui(player_index)
+	local player = game.players[player_index]
+	local changes = gui.get_changes_from_ui(player_index)
+
+	if changes == nil then
+		return
+	end
+
+	blueprints.backfill_changes(changes)
+	local base_blueprint = globalstore.get_value(player_index, "last_selected_blueprint")
+	local modified_blueprint = blueprints.modify_blueprint(base_blueprint, changes)
+	blueprints.give_player_blueprint(player, modified_blueprint)
+	gui.close_options_gui(player_index)
 end
 
-gui.confirm_selection_if_all_are_set = function (player_index)
-    -- check settings to see if we should continue
-    if not settings.player["parameterized-blueprints-use-quick-blueprint-confirm"] then
-        return
-    end
-    
-    -- check if all are set, if so then confirm blueprint
-    local content_flow = globalstore.get_value(player_index, "options_gui")["frame"][gui.options_content_flow_name]
-    for _, entry in ipairs(content_flow.children) do
-        -- don't check for visiblity - will need to grab value stored here even if it's not visible
-        local recipe_button = entry["target_recipe_button"]
-        local item_button = entry["target_item_button"]
+gui.confirm_selection_if_all_are_set = function(player_index)
+	-- check settings to see if we should continue
+	if not settings.player["parameterized-blueprints-use-quick-blueprint-confirm"] then
+		return
+	end
 
-        -- if any elements are not set then don't do it
-        if item_button.elem_value == nil then
-            return
-        end
+	-- check if all are set, if so then confirm blueprint
+	local content_flow = globalstore.get_value(player_index, "options_gui")["frame"][gui.options_content_flow_name]
+	for _, entry in ipairs(content_flow.children) do
+		-- don't check for visiblity - will need to grab value stored here even if it's not visible
+		local recipe_button = entry["target_recipe_button"]
+		local item_button = entry["target_item_button"]
 
-        -- skip to next if the recipe button is not visible
-        if not recipe_button.visible then
-            goto continue
-        end
+		-- if any elements are not set then don't do it
+		if item_button.elem_value == nil then
+			return
+		end
 
-        -- if the recipe button is visible but not set then don't do it
-        if recipe_button.elem_value == nil then
-            return
-        end
+		-- skip to next if the recipe button is not visible
+		if not recipe_button.visible then
+			goto continue
+		end
 
-        ::continue::
-    end
+		-- if the recipe button is visible but not set then don't do it
+		if recipe_button.elem_value == nil then
+			return
+		end
 
-    -- if all checks pass then confirm the selection
-    game.players[player_index].print("blueprint confirmed!")
-    gui.confirm_selection(player_index)
+		::continue::
+	end
+
+	-- if all checks pass then confirm the selection
+	game.players[player_index].print("blueprint confirmed!")
+	gui.confirm_selection(player_index)
 end
-
 
 return gui
